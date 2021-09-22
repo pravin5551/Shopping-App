3befcdd43337135b0b52b5c42e003ccb04a791c3
Pravin Pawar <pravinvpawar5551@gmail.com>
2021-09-22 00:18:49 -0700

    product_item.dart added

--- shopping_app/lib/widgets/product_item.dart
+++ shopping_app/lib/widgets/product_item.dart
@@ -1,47 +1,72 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:shopping_app/providers/cart.dart';
+import 'package:shopping_app/providers/product.dart';
 import 'package:shopping_app/screens/product_details_screen.dart';
+// import 'package:shop_app/providers/cart.dart';
+// import 'package:shop_app/providers/product.dart';
+// import 'package:shop_app/screens/product_details_screen.dart';
 
 class ProductItem extends StatelessWidget {
-  final String id;
-  final String title;
-  final String imageUrl;
-  final double price;
-
-  const ProductItem(this.id, this.title, this.imageUrl, this.price);
-
   @override
   Widget build(BuildContext context) {
+    final cart = Provider.of<Cart>(context, listen: false);
+    final product = Provider.of<Product>(context,
+        listen:
+            false); // listener: false - if we dont wanted to get update from provider
     return ClipRRect(
       borderRadius: BorderRadius.circular(10),
       child: GridTile(
         child: GestureDetector(
           onTap: () {
             Navigator.of(context).pushNamed(
-                ProductDetailScreen.routName, arguments: id,);
+              ProductDetailScreen.routename,
+              arguments: product.id,
+            );
           },
           child: Image.network(
-            imageUrl,
+            product.imageUrl,
             fit: BoxFit.cover,
           ),
         ),
         footer: GridTileBar(
-          leading: IconButton(
-            icon: Icon(Icons.favorite),
-            onPressed: () {},
-            color: Colors.red,
-          ),
           backgroundColor: Colors.black54,
+          leading: Consumer<Product>(
+            // Consumer is same as Provider.of - we can use consumer if we wanted to change specific widget insted of whole build method by wrapping consumer
+            builder: (context, product, child) => IconButton(
+              color: Theme.of(context).accentColor,
+              icon: Icon(
+                  product.isFavroite ? Icons.favorite : Icons.favorite_border),
+              onPressed: () {
+                product.toggleFavroiteStatus();
+              },
+            ),
+          ),
           title: Text(
-            title,
+            product.title,
             textAlign: TextAlign.center,
           ),
           trailing: IconButton(
-            icon: Icon(Icons.add_shopping_cart),
-            onPressed: () {},
+            color: Theme.of(context).accentColor,
+            icon: Icon(Icons.shopping_cart),
+            onPressed: () {
+              cart.addItem(product.id, product.price, product.title);
+              Scaffold.of(context).hideCurrentSnackBar();
+              Scaffold.of(context).showSnackBar(
+                SnackBar(
+                  content: Text('Added item to card!'),
+                  duration: Duration(seconds: 2),
+                  action: SnackBarAction(
+                    label: 'Undo',
+                    onPressed: () {
+                      cart.removeSingleItem(product.id);
+                    },
+                  ),
+                ),
+              );
+            },
           ),
         ),
-
       ),
     );
   }
