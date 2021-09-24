be51b89ed18fa336a31635e6dfcab8c6b83753f8
Pravin Pawar <pravinvpawar5551@gmail.com>
2021-09-24 09:39:09 -0700

    user_product_item.dart added - 1

--- shopping_app/lib/widgets/user_product_item.dart
+++ shopping_app/lib/widgets/user_product_item.dart
@@ -1,10 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:shopping_app/screens/edit_product_screen.dart';
 
 class UserProductItem extends StatelessWidget {
   final String title;
   final String imageUrl;
+  final String id;
 
-  UserProductItem({required this.title, required this.imageUrl});
+  UserProductItem({required this.title, required this.imageUrl, required this.id});
 
   @override
   Widget build(BuildContext context) {
@@ -18,7 +20,9 @@ class UserProductItem extends StatelessWidget {
         child: Row(
           children: [
             IconButton(
-              onPressed: () {},
+              onPressed: () {
+                Navigator.of(context).pushNamed(EditProductScreen.routeName, arguments: id);
+              },
               icon: Icon(Icons.edit),
               color: Theme.of(context).primaryColor,
             ),
